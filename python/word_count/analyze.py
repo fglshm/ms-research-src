37a532deb0e031468342e13437755612b3ac0c15
前野翔平 <fglshm@gmail.com>
2019-10-23 21:56:16 +0900

    update

--- python/word_count/analyze.py
+++ python/word_count/analyze.py
@@ -5,7 +5,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 
 
 class Data:
@@ -27,11 +26,12 @@ class Analyzer:
         1スピーチの中で, 前後の文の単語数の違いが顕著な文のペアを取得するクラス
     """
 
-    def __init__(self, similarities):
+    def __init__(self, similarities, threshold):
         self.similarities = similarities
         self.prev_values = self.similarities
         self.N = self.similarities.shape[0]
         self.frequency = np.zeros(10)
+        self.threshold = threshold
 
     def calculate(self):
         """
@@ -44,7 +44,7 @@ class Analyzer:
             over_avg_idx = np.where(sentence > avg)
             over_avg = sentence[over_avg_idx]
             self.prev_values = over_avg
-            if len(over_avg) < self.N / 20:
+            if len(over_avg) < self.N / self.threshold:
                 return avg, over_avg_idx[0], over_avg
 
     def position(self, values):
@@ -68,18 +68,29 @@ class Analyzer:
 
 
 def main():
-    data = Data("nso")
-    frequencies = np.zeros(10)
-    sent_num = 0
-    for similarity in tqdm(data.similarities):
-        analyzer = Analyzer(similarity)
-        sent_num += analyzer.N
-        avg, idx, values = analyzer.calculate()
-        position = analyzer.position(idx)
-        analyzer.get_frequency(position)
-        frequencies = np.add(frequencies, analyzer.frequency)
-    print(frequencies)
-    print(sent_num)
+    save_path = '/Users/shohei/Documents/Tsukuba/Research/results/similarity/so_nso_diff'
+    for speech_type in ['so', 'nso']:
+        data = Data(speech_type)
+        frequencies = np.zeros(10)
+        sent_num = 0
+        threshold = 20
+        for similarity in data.similarities:
+            analyzer = Analyzer(similarity, threshold)
+            sent_num += analyzer.N
+            avg, idx, values = analyzer.calculate()
+            position = analyzer.position(idx)
+            analyzer.get_frequency(position)
+            frequencies = np.add(frequencies, analyzer.frequency)
+        print(
+            f'{speech_type}: {np.divide(frequencies, sent_num)} ({threshold})')
+        plt.plot(np.divide(frequencies, sent_num), label=speech_type)
+    plt.xlabel('position')
+    plt.ylabel('frequency')
+    plt.title(f"threshold {threshold}")
+    plt.legend()
+    figure = plt.gcf()
+    figure.set_size_inches(20, 10)
+    plt.savefig(f"{save_path}/diff_peak_{threshold}.pdf", format="pdf")
 
 
 if __name__ == "__main__":
